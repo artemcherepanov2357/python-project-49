3eca80eee65b1e61889563461211d838b464718c
Artem Cherepanov <artemcherepanov2357@gmail.com>
2025-07-01 13:02:46 +0000

    nosec - not security sensitive

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,13 +6,13 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(number):
     if number < 2:
         return False
-    for i in range(2, int(number ** 0.5) + 1):
+    for i in range(2, int(number**0.5) + 1):
         if number % i == 0:
             return False
     return True
 
 
 def generate_round():
-    question = random.randint(2, 100)
+    question = random.randint(2, 100)  # nosec - not security sensitive
     correct_answer = "yes" if is_prime(question) else "no"
     return str(question), correct_answer
